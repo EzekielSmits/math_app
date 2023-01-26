10eadad6a2da0335874ec8ec3c083ab2bc9ea218
EzekielSmits <123680420+EzekielSmits@users.noreply.github.com>
2023-01-26 13:03:45 -0800

    Deleted unused functions, added new functions; GetAnswer + ReturnOperator

--- main.ts
+++ main.ts
@@ -1,40 +1,30 @@
-function ShowOperator () {
-    if (operator == 1) {
-        basic.showString("x")
-    }
-    if (operator == 2) {
-        basic.showString("+")
-    }
-    if (operator == 3) {
-        basic.showString("-")
-    }
-    if (operator == 4) {
-        basic.showString("/")
-    }
-}
 input.onButtonPressed(Button.A, function () {
     int1 = randint(0, 10)
     int2 = randint(0, 10)
-    basic.showNumber(int1)
-    basic.pause(1000)
-    ShowOperator()
-    basic.pause(1000)
-    basic.showNumber(int2)
-    basic.pause(1000)
-    basic.showString("= ?")
+    ReturnOperator()
+    basic.showString("" + int1 + currentOperator + int2)
 })
+function ReturnOperator () {
+    if (operator == 1) {
+        currentOperator = "x"
+    }
+    if (operator == 2) {
+        currentOperator = "+"
+    }
+    if (operator == 3) {
+        currentOperator = "-"
+    }
+    if (operator == 4) {
+        currentOperator = "/"
+    }
+}
 input.onButtonPressed(Button.AB, function () {
     int1 = randint(0, 100)
     int2 = randint(0, 100)
-    basic.showNumber(int1)
-    basic.pause(1000)
-    ShowOperator()
-    basic.pause(1000)
-    basic.showNumber(int2)
-    basic.pause(1000)
-    basic.showString("= ?")
+    ReturnOperator()
+    basic.showString("" + int1 + currentOperator + int2)
 })
-input.onButtonPressed(Button.B, function () {
+function GetAnswer () {
     if (operator == 1) {
         basic.showNumber(int1 * int2)
     }
@@ -47,6 +37,9 @@ input.onButtonPressed(Button.B, function () {
     if (operator == 4) {
         basic.showNumber(int1 / int2)
     }
+}
+input.onButtonPressed(Button.B, function () {
+    GetAnswer()
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     if (operator <= 3) {
@@ -55,6 +48,7 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
         operator += -1
     }
 })
+let currentOperator = ""
 let int2 = 0
 let int1 = 0
 let operator = 0
